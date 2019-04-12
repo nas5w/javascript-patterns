0dc7aa08025ffe28763047602fe8a1c8cb871b65
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2019-04-12 16:41:43 -0400

    Finish tests for mergeSort

--- test/algorithms/sorting/mergeSort.test.js
+++ test/algorithms/sorting/mergeSort.test.js
@@ -1,17 +1,32 @@
 const mergeSort = require("../../../algorithms/sorting/mergeSort");
-
-const compareArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false;
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-
-  return true;
-};
+const compareArrays = require("../../helpers/compareArrays");
 
 test("Sorted array remains in order", () => {
   const arr1 = [1, 2, 3, 4, 5];
   const arr2 = mergeSort(arr1);
   expect(compareArrays(arr1, arr2)).toBe(true);
+});
+
+test("Unsorted array is sorted", () => {
+  const arr1 = [5, 2, 3, 1, 4];
+  const arr2 = mergeSort(arr1);
+  expect(compareArrays([1, 2, 3, 4, 5], arr2)).toBe(true);
+});
+
+test("Single item array is unchanged", () => {
+  const arr1 = [5];
+  const arr2 = mergeSort(arr1);
+  expect(compareArrays(arr1, arr2)).toBe(true);
+});
+
+test("Empty array is unchanged", () => {
+  const arr1 = [];
+  const arr2 = mergeSort(arr1);
+  expect(compareArrays(arr1, arr2)).toBe(true);
+});
+
+test("Negative array is sorted", () => {
+  const arr1 = [-5, -1, -10, -15, -20];
+  const arr2 = mergeSort(arr1);
+  expect(compareArrays([-20, -15, -10, -5, -1], arr2)).toBe(true);
 });
---
+++ test/helpers/compareArrays.js
@@ -0,0 +1,11 @@
+const compareArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+};
+
+module.exports = compareArrays;
