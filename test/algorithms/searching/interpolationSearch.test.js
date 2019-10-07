6dffba2362f142ba03df2250594aabe5e3e68947
mohammed-almassri <mohammed.m.almassri@gmail.com>
2019-10-07 14:54:04 +0300

    added more tests for interpolation search #31

--- test/algorithms/searching/interpolationSearch.test.js
+++ test/algorithms/searching/interpolationSearch.test.js
@@ -6,4 +6,14 @@ describe("Interpolation Search", () => {
     const result = interpolationSearch(arr, 8);
     expect(result).toBe(4);
   });
+  it("Finds item in arrays of length 1", () => {
+    const arr = [1];
+    const result = interpolationSearch(arr, 1);
+    expect(result).toBe(0);
+  });
+  it("returns NaN when the item does not exist", () => {
+    const arr = [1, 3, 5, 7, 8, 9];
+    const result = interpolationSearch(arr, 2);
+    expect(result).toBe(NaN);
+  });
 });
