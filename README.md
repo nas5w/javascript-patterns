b796080f401a9713c033d2367f6bdf5949abc580
Abdelrahman Ashraf <a.theashraf@gmail.com>
2019-10-08 01:57:48 +0200

    feat: 🎸 Bridge Pattern (#74)
    adding bridge pattern and its associated tests

--- README.md
+++ README.md
@@ -54,7 +54,7 @@ This repository maintained by [Nick Scialli](https://twitter.com/nas5w) and powe
 - [Builder](https://github.com/nas5w/javascript-patterns/blob/master/src/patterns/builder.js)
 - TODO: Factory
 - TODO: Adapter
-- TODO: Bridge
+- [Bridge](/src/patterns/bridge.js)
 - Decorator
 - TODO: Flyweight
 - TODO: Proxy
---
+++ test/patterns/bridge.test.js
@@ -0,0 +1,39 @@
+const Bridge = require("../../src/patterns/bridge");
+
+test("Implementors should have the same interface but different behavior", () => {
+  const { Implementors } = Bridge;
+
+  const redColor = new Implementors.Red();
+  const blueColor = new Implementors.Blue();
+
+  expect(Object.keys(redColor)).toEqual(Object.keys(blueColor));
+  expect(redColor.getColorName()).not.toBe(blueColor.getColorName());
+});
+
+test("Abstractions may have different interface", () => {
+  const { Abstractions } = Bridge;
+
+  const circle = new Abstractions.Circle();
+  const rectangle = new Abstractions.Rectangle();
+
+  expect(Object.keys(circle)).not.toEqual(Object.keys(rectangle));
+});
+
+test("Allows two components to work together with each component having its own interface", () => {
+  const { Abstractions, Implementors } = Bridge;
+
+  const redColor = new Implementors.Red();
+  const blueColor = new Implementors.Blue();
+
+  const redCircle = new Abstractions.Circle(redColor);
+  expect(redCircle.plot()).toMatch(/red circle/i);
+
+  const blueCircle = new Abstractions.Circle(blueColor);
+  expect(blueCircle.plot()).toMatch(/blue circle/i);
+
+  const redRectangle = new Abstractions.Rectangle(redColor);
+  expect(redRectangle.draw()).toMatch(/red rectangle/i);
+
+  const blueRectangle = new Abstractions.Rectangle(blueColor);
+  expect(blueRectangle.draw()).toMatch(/blue rectangle/i);
+});
