0afb0ac39bdbc3f2ae352ec3a30e43d058ff4353
Abdelrahman Ashraf <a.theashraf@gmail.com>
2019-10-07 22:47:33 +0200

    test: 💍 revealing module pattern (#51)
    * test: 💍 revealing module pattern
    
    Closes: #50
    
    * docs: ✏️ update readme
    
    clear (Tests needed) from Revealing Module

---
+++ test/patterns/revealing-module.test.js
@@ -0,0 +1,14 @@
+const revealingModule = require("../../src/patterns/revealing-module");
+
+test("shouldn't expose privateFunction", () => {
+  expect(revealingModule.privateFunction).not.toBeDefined();
+});
+
+test("exposes a publicFunction", () => {
+  expect(typeof revealingModule.publicFunction).toBe("function");
+});
+
+test("access privateFunction from exposePrivateFunction", () => {
+  const result = revealingModule.exposePrivateFunction();
+  expect(result).toMatch(/this is a private function/i);
+});
