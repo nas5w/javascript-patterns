7caff5797c415d9c806f0a51c6ba64697680b0ca
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2019-10-06 21:50:01 -0400

    Fix countingSort variable declaration

--- src/algorithms/sorting/countingSort.js
+++ src/algorithms/sorting/countingSort.js
@@ -1,23 +1,24 @@
-function countingSort(arr)
-  {
-    min = Math.min(...arr)
-    max = Math.max(...arr)
-    var i, z = 0, count = [];
- 
-    for (i = min; i <= max; i++) {
-        count[i] = 0;
+function countingSort(arr) {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  var i,
+    z = 0,
+    count = [];
+
+  for (i = min; i <= max; i++) {
+    count[i] = 0;
+  }
+
+  for (i = 0; i < arr.length; i++) {
+    count[arr[i]]++;
+  }
+
+  for (i = min; i <= max; i++) {
+    while (count[i]-- > 0) {
+      arr[z++] = i;
     }
- 
-    for (i=0; i < arr.length; i++) {
-        count[arr[i]]++;
-    }
- 
-    for (i = min; i <= max; i++) {
-        while (count[i]-- > 0) {
-            arr[z++] = i;
-        }
-    }
- return arr;
+  }
+  return arr;
 }
 
 module.exports = countingSort;
