59fce7e5ce258bbd9a5df2d1a76b76ab8e1e6682
Abdul Rehman <abdul.rehman@ymedialabs.com>
2019-10-08 13:05:15 +0530

    a bit more tests, a bit more coverage

--- test/algorithms/searching/binarySearch.test.js
+++ test/algorithms/searching/binarySearch.test.js
@@ -2,9 +2,15 @@ const binarySearch = require('../../../src/algorithms/searching/binarySearch');
 const compareArrays = require('../../helpers/compareArrays');
 
 describe('Binary Search algorithm', () => {
-    it('Find item in array', () => {
-        const arr1 = [1, 3, 5, 7, 8, 9];
-        const result = binarySearch(arr1, 8);
-        expect(result).toBe(true);
-    });
+  it('Find item in array', () => {
+    const arr1 = [1, 3, 5, 7, 8, 9];
+    const result = binarySearch(arr1, 8);
+    expect(result).toBe(true);
+  });
+
+  it('Item not found in an array', () => {
+    const arr1 = [1, 3, 5, 7, 8, 9];
+    const result = binarySearch(arr1, 12);
+    expect(result).toBeFalsy();
+  });
 });
--- test/patterns/revealing-module.test.js
+++ test/patterns/revealing-module.test.js
@@ -1,14 +1,19 @@
-const revealingModule = require("../../src/patterns/revealing-module");
+const revealingModule = require('../../src/patterns/revealing-module');
 
 test("shouldn't expose privateFunction", () => {
   expect(revealingModule.privateFunction).not.toBeDefined();
 });
 
-test("exposes a publicFunction", () => {
-  expect(typeof revealingModule.publicFunction).toBe("function");
+test('exposes a publicFunction', () => {
+  expect(typeof revealingModule.publicFunction).toBe('function');
 });
 
-test("access privateFunction from exposePrivateFunction", () => {
+test('access privateFunction from exposePrivateFunction', () => {
   const result = revealingModule.exposePrivateFunction();
   expect(result).toMatch(/this is a private function/i);
+});
+
+test('access publicFunction from publicFunction', () => {
+  const result = revealingModule.publicFunction();
+  expect(result).toMatch(/this is a public function/i);
 });
