69e94feb7c01bf5c5ae45cf675628e427224ad2b
Shantanu Shinde <shinde.shantanu21@gmail.com>
2019-10-08 10:23:24 +0530

    updated fibonacciSearch.test.js

--- test/algorithms/searching/fibonacciSearch.test.js
+++ test/algorithms/searching/fibonacciSearch.test.js
@@ -2,9 +2,18 @@ const fibonacciSearch = require('../../../src/algorithms/searching/fibonacciSear
 const compareArrays = require('../../helpers/compareArrays');
 
 describe('Fibonacci Search algorithm', () => {
-    it('Find item in array', () => {
+    it('Find existing item in an array', () => {
         const arr = [2, 4, 10, 20, 35, 67];
         const result = fibonacciSearch(arr, 10);
         expect(result).toBe(2);
     });
+    it('Find non-existing item in an array', () => {
+        const arr = [2, 4, 10, 20, 35, 67];
+        const result = fibonacciSearch(arr, 30);
+        expect(result).toBe(-1);
+    });
+    it('Find existing item in an array', () => {
+        const arr = [20, 25, 30, 56, 100, 120, 135];
+        const result = fibonacciSearch(arr, 56);
+        expect(result).toBe(3);
 });
