b19c831ac4ec2ff3f69453b38350ce5f89837bcb
Nick Wang <nicholas.wang.h@gmail.com>
2019-11-30 17:50:26 +1100

    added test cases for rise and fall methods

--- test/dataStructures/Heap.test.js
+++ test/dataStructures/Heap.test.js
@@ -8,13 +8,21 @@ describe('Heap data structure', () => {
 });
 
 describe('Heap private method rise', () => {
-	it('should not move items in the correct position', () => {
+	it('should not move top item', () => {
 		const heap = new Heap();
 		heap._storage[1] = 1;
 		heap._storage[2] = 2;
-		heap._storage[3] = 3;
 		heap._rise(1);
-		expect(heap._storage).toEqual([undefined, 1, 2, 3]);
+		expect(heap._storage).toEqual([undefined, 1, 2]);
+	});
+
+	it('should not move items in the correct position', () => {
+		const heap = new Heap();
+		heap._storage[1] = 2;
+		heap._storage[2] = 1;
+		heap._storage[3] = 3;
+		heap._rise(2);
+		expect(heap._storage).toEqual([undefined, 2, 1, 3]);
 	});
 
 	it('should move large items up', () => {
@@ -30,6 +38,15 @@ describe('Heap private method rise', () => {
 });
 
 describe('Heap private method fall', () => {
+	it('should not move items with no children', () => {
+		const heap = new Heap();
+		heap._storage[1] = 1;
+		heap._storage[2] = 2;
+		heap._storage[3] = 3;
+		heap._fall(3);
+		expect(heap._storage).toEqual([undefined, 1, 2, 3]);
+	});
+
 	it('should not move items in the correct position', () => {
 		const heap = new Heap();
 		heap._storage[1] = 4;
