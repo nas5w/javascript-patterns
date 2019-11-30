8f07121f9ae3578863388600da69d415e75a5050
Nick Wang <nicholas.wang.h@gmail.com>
2019-11-30 17:07:08 +1100

    heap is now only max heap

--- src/dataStructures/Heap.js
+++ src/dataStructures/Heap.js
@@ -1,9 +1,8 @@
 class Heap {
-	constructor(size = 15, isMax = 1) {
+	constructor(size = 15) {
 		this._storage = new Array(size + 1); // 'size' items and one shift item to make calculations easier
 		this._sizeCounter = 3; // size = 2^this._sizeCounter - 1
 		this._last = null; // points to the last item in the heap
-		this._isMax = isMax; // Max Heap or Min Heap
 	}
 
 	/**
@@ -25,20 +24,27 @@ class Heap {
      */
 	_rise(index) {
 		while (index > 1) {
-			let parentIndex = Math.floor(index/2); // child items are 2*index and 2*index + 1
+			const parentIndex = Math.floor(index/2); // child items are 2*index and 2*index + 1
 			// swap if < for max heap, > for min heap
-			if (
-				(this._isMax && this._storage[parentIndex] < this._storage[index]) // max heap
-				|| (!this._isMax && this._storage[parentIndex] > this._storage[index]) // min heap
-			) {
+			if (this._storage[parentIndex] < this._storage[index]) {
 				this._swap(parentIndex, index)
 			}
 			index = parentIndex;
 		}
 	}
 
+	/**
+     * Moves item down the heap to the correct position
+	 *
+     * @param {Number} index
+     * @returns {void}
+     */
 	_fall(index) {
-
+		while (index < this._storage.length) {
+			const c1 = 2*index; // left child
+			const c2 = 2*index + 1; // right child
+			let childIndex;
+		}
 	}
 
 	isEmpty() {
--- test/dataStructures/Heap.test.js
+++ test/dataStructures/Heap.test.js
@@ -8,8 +8,8 @@ describe('Heap data structure', () => {
 });
 
 describe('Heap private method rise', () => {
-	it('should move large items up for max Heap', () => {
-		const heap = new Heap(3, true);
+	it('should move large items up', () => {
+		const heap = new Heap(3);
 		heap._storage[1] = 1;
 		heap._storage[2] = 2;
 		heap._storage[3] = 3;
@@ -19,18 +19,5 @@ describe('Heap private method rise', () => {
 		expect(heap._storage).toEqual([undefined, 2, 1, 3]);
 		heap._rise(3);
 		expect(heap._storage).toEqual([undefined, 3, 1, 2]);
-	});
-
-	it('should move small items up for min Heap', () => {
-		const heap = new Heap(3, false);
-		heap._storage[1] = 3;
-		heap._storage[2] = 2;
-		heap._storage[3] = 1;
-		heap._rise(1);
-		expect(heap._storage).toEqual([undefined, 3, 2, 1]);
-		heap._rise(2);
-		expect(heap._storage).toEqual([undefined, 2, 3, 1]);
-		heap._rise(3);
-		expect(heap._storage).toEqual([undefined, 1, 3, 2]);
 	});
 });
