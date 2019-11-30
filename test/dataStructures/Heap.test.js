8fef6440099d1ad0bdaf9ae60ecb56ed439a9975
Nick Wang <nicholas.wang.h@gmail.com>
2019-11-30 16:33:55 +1100

    tests for rise method

--- test/dataStructures/Heap.test.js
+++ test/dataStructures/Heap.test.js
@@ -6,3 +6,31 @@ describe('Heap data structure', () => {
 		expect(heap._storage).toEqual([undefined, undefined, undefined, undefined]);
 	});
 });
+
+describe('Heap private method rise', () => {
+	it('should move large items up for max Heap', () => {
+		const heap = new Heap(3, true);
+		heap._storage[1] = 1;
+		heap._storage[2] = 2;
+		heap._storage[3] = 3;
+		heap._rise(1);
+		expect(heap._storage).toEqual([undefined, 1, 2, 3]);
+		heap._rise(2);
+		expect(heap._storage).toEqual([undefined, 2, 1, 3]);
+		heap._rise(3);
+		expect(heap._storage).toEqual([undefined, 3, 1, 2]);
+	});
+
+	it('should move small items up for min Heap', () => {
+		const heap = new Heap(3, false);
+		heap._storage[1] = 3;
+		heap._storage[2] = 2;
+		heap._storage[3] = 1;
+		heap._rise(1);
+		expect(heap._storage).toEqual([undefined, 3, 2, 1]);
+		heap._rise(2);
+		expect(heap._storage).toEqual([undefined, 2, 3, 1]);
+		heap._rise(3);
+		expect(heap._storage).toEqual([undefined, 1, 3, 2]);
+	});
+});
